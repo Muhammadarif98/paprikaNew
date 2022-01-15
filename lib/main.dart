723c87585ff727713cc6393e3d173f94c7a36f6e
Muhammadarif98 <67822369+Muhammadarif98@users.noreply.github.com>
2022-01-16 00:09:33 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/paprikaapp/generatedframe118widget/GeneratedFrame118Widget.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(title: 'Paprika'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+          children: [
+            Center(
+              child:GeneratedFrame118Widget(countShop: 0),
+            )
+          ],
+        )
+        ),
+    );
+  }
+}
+
